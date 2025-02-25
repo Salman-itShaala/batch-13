4061dd90811df91ce90208bb78329a83fc9d02f3
salman-itShaala <salman.shaikh@itshaala.com>
2025-02-25 11:11:54 +0530

    added code

---
+++ js/day -21/script.js
@@ -0,0 +1,37 @@
+const userName = "Salman";
+
+console.log(userName.length);
+
+console.log(userName.at(0));
+
+console.log(userName.endsWith("man")); // true , false
+
+console.log(userName.includes("a"));
+
+console.log(userName.replace("a", "e"));
+
+console.log(userName.toLowerCase());
+
+console.log(userName.toUpperCase());
+
+let userChoice = "   rock    "; // rock
+
+console.log(userChoice.trim());
+
+let intro = `My name is ${userName}`;
+
+// " Select * from users where userId =  " +  23;
+
+// Insert into products values( + "," + ,,)
+
+console.log(intro);
+
+console.log(intro.split("a"));
+
+// print all the characters in given string
+
+// Ant --> A n t
+
+// Salman
+
+console.log(userName.substring(1));
