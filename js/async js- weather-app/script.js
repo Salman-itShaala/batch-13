7c51f229c857cb2accc0c1d4cb78b3c7a8f1695d
salman-itShaala <salman.shaikh@itshaala.com>
2025-03-05 09:05:38 +0530

    partially completed weeather app

--- js/async js- weather-app/script.js
+++ js/async js- weather-app/script.js
@@ -1,17 +1,27 @@
-// key = e9a96cfc5c981bda8bb9754bd37a2503
-
-// http://api.weatherstack.com/current?access_key=a849f1413213dea07377caf6d6323584&query=New York
+const cityInput = document.getElementById("city-input");
+const cityWrapper = document.getElementById("city-wrapper");
+const weatherImg = document.getElementById("weather-img");
 
 function sendReq() {
+  const city = cityInput.value;
+
   const promise = fetch(
-    "https://api.weatherstack.com/current?access_key=79851ffaf00d1ddb562a5ce58338f114&query=pune"
+    `https://api.weatherstack.com/current?access_key=79851ffaf00d1ddb562a5ce58338f114&query=${city}`
   );
 
   promise
     .then((res) => res.json())
     .then((data) => {
-      // show this data on front end
-      console.log(data);
+      const resultCity = data.request.query;
+      const resultTemp = data.current.temperature;
+      const weatherImgUrl = data.current.weather_icons[0];
+      const weatherDesc = data.current.weather_descriptions[0];
+      const resultPressure = data.current.pressure;
+      const resultPrec = data.current.precip;
+
+      // update everything on front-end
+      cityWrapper.innerText = resultCity;
+      weatherImg.setAttribute("src", weatherImgUrl);
     })
     .catch((err) => console.log(err));
 }
