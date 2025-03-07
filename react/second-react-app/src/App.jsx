80d3fadd6ddf29ff4434173c04a771916be3dca9
salman-itShaala <salman.shaikh@itshaala.com>
2025-03-07 10:03:57 +0530

    added second react app

---
+++ react/second-react-app/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Header from "./Header";
+import MyComponent from "./MyComponent";
+
+function App() {
+  return (
+    <div className="my-div">
+      <Header />
+      <h1>Hello World</h1>
+
+      <MyComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ react/second-react-app/src/MyComponent.jsx
@@ -0,0 +1,12 @@
+function MyComponent() {
+  // logic
+
+  return (
+    <>
+      <h1>I'm your component</h1>
+      <h1>Another h1</h1>
+    </>
+  );
+}
+
+export default MyComponent;
